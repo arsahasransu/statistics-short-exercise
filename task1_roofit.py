9258b3a96fb0d841737d7d15d1b403f41c961c65
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2020-09-07 14:46:37 +0200

    End of Task1

--- task1_roofit.py
+++ task1_roofit.py
@@ -7,11 +7,13 @@ sigma = RooRealVar("sigma", "variance in mass of decay particles in GeV", 10, 0,
 #MH.Print()
 #print MH.getVal()
 
+# Define a function and a gaussian PDF
 func = RooFormulaVar("R", "@0/@1", RooArgList(sigma,mass))
 gauss = RooGaussian("gauss", "f(m|M_{H},#sigma)", mass, MH, sigma)
 #func.Print("v")
 #gauss.Print("v")
 
+# Plotting the PDF and functions
 can = TCanvas()
 plot = mass.frame()
 
@@ -26,3 +28,9 @@ gauss.plotOn(plot,RooFit.LineColor(kGreen),RooFit.LineStyle(2))
 plot.Draw()
 can.Draw()
 can.SaveAs("gaussians.pdf")
+
+# Saving to Workspace
+w = RooWorkspace("w")
+getattr(w,'import')(gauss)
+getattr(w,'import')(func)
+w.writeToFile("workspace.root")
